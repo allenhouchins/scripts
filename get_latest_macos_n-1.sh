0cba3cf01da65c896d2cf9a0341bb77b329f7b73
Allen Houchins <allenhouchins@mac.com>
2024-09-19 21:48:47 -0500

    Update get_latest_macos_n-1.sh

--- get_latest_macos_n-1.sh
+++ get_latest_macos_n-1.sh
@@ -1,14 +1,29 @@
 #!/bin/sh
 
 # Fetch the JSON data and extract the ProductVersion strings
-versions=$(/usr/bin/curl -s "https://sofafeed.macadmins.io/v1/macos_data_feed.json" | \
-/usr/bin/jq -r '.. | objects | select(has("ProductVersion")) | .ProductVersion')
+versions=$(curl -s "https://sofafeed.macadmins.io/v1/macos_data_feed.json" | \
+jq -r '.. | objects | select(has("ProductVersion")) | .ProductVersion')
 
 # Find the two highest major versions
-highest_two_majors=$(echo "$versions" | /usr/bin/cut -d '.' -f1 | /usr/bin/sort -Vr | /usr/bin/uniq | /usr/bin/head -n 2)
+highest_two_majors=$(echo "$versions" | cut -d '.' -f1 | sort -Vr | uniq | head -n 2)
+
+# Initialize an array to store the highest versions
+highest_versions=()
 
 # Extract the highest version for each of the two highest major versions
 for major in $highest_two_majors; do
-  highest_version=$(echo "$versions" | /usr/bin/grep "^$major\." | /usr/bin/sort -Vr | /usr/bin/head -n 1)
-  echo "$highest_version"
+  highest_version=$(echo "$versions" | grep "^$major\." | sort -Vr | head -n 1)
+  highest_versions+=("$highest_version")
 done
+
+# Construct the SQL query string with the highest versions
+sql_query="SELECT 1 FROM os_version WHERE version >= '${highest_versions[0]}' OR version >= '${highest_versions[1]}';"
+
+# Path to the YAML file
+yml_file="../lib/operating-system-up-to-date-macos.yml"
+
+# Use sed to replace the line that starts with 'query:' with the new SQL query
+sed -i.bak "s|^query:.*|query: \"$sql_query\"|" "$yml_file"
+
+# Output a success message
+echo "The query in the YAML file has been updated successfully."
