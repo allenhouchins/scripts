4800e0afa0bf1804480f9c203ae8eb10722cc748
Allen Houchins <allenhouchins@mac.com>
2024-09-21 19:00:41 -0500

    Update get_latest_google_chrome_for_macos.sh

--- get_latest_google_chrome_for_macos.sh
+++ get_latest_google_chrome_for_macos.sh
@@ -4,7 +4,11 @@
 url="https://raw.githubusercontent.com/berstend/chrome-versions/master/data/stable/mac/version/latest.json"
 
 # Fetch the JSON data and extract the version field
-version=$(curl -s "$url" | jq -r '.version')
+latest_chrome_version=$(curl -s "$url" | jq -r '.version')
 
 # Output the version
-echo "Chrome stable version: $version"
+echo "Chrome stable version: "$latest_chrome_version""
+
+
+# Write updated Google Chrome policy
+echo "SELECT 1 FROM apps WHERE name = "Google Chrome.app" AND version_compare(bundle_short_version, '"$latest_chrome_version"') >= 0;"
