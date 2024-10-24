ec19fe987da21ac95f34abeb30478e11707949e4
Allen Houchins <allenhouchins@mac.com>
2024-10-23 21:35:44 -0500

    Update remove_jamf.sh

--- remove_jamf.sh
+++ remove_jamf.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
-# This script runs one last recon, updates the JSS API to show the machine is unmanaged, and then creates a launchd to remove the jamf framework.
-
-# Define variables
-apiUsername="apiusername"
-apiPassword="password"
-jssServer="https://server.jamfcloud.com"
-name=`scutil --get ComputerName`
-curlName=`echo "$name"|sed 's/ /%20/g'`
-xmlString="<?xml version="1.0" encoding="UTF-8"?><computer><general><remote_management><managed>false</managed><management_username/></remote_management></general></computer>"
+# This script runs one last recon and then removes the jamf binary. This should be distributed by any means other than Jamf Pro and must be run as root.
 
 # Identify the location of the jamf binary for the jamf_binary variable.
 CheckBinary (){
@@ -20,7 +12,12 @@ CheckBinary (){
         jamf_binary="/usr/local/bin/jamf"
     elif [[ "$jamf_binary" == "" ]] && [[ -e "/usr/sbin/jamf" ]] && [[ -e "/usr/local/bin/jamf" ]]; then
         jamf_binary="/usr/local/bin/jamf"
+    else
+        echo "Jamf binary not found. Exiting..."
+        exit 0
     fi
+
+    echo "The jamf binary is installed at $jamf_binary"
 }
 
 # Update the computer inventory
@@ -28,45 +25,9 @@ RunRecon (){
     $jamf_binary recon
 }
 
-# Use the API to update the management status of the computer
-# Takes a parameter for the computer name
-UpdateAPI (){
-    curl -sS -k -i -u ${apiUsername}:${apiPassword} -X PUT -H "Content-Type: text/xml" -d "${xmlString}" ${jssServer}/JSSResource/computers/name/`perl -MURI::Escape -e 'print uri_escape($ARGV[0]);' "$name"`
-}
-
-# Create a temp launchd job in /private/tmp/ that uses a RunAtLoad key of false and StartInterval of 60 seconds
-# This is done so that the script can return a result to the JSS before the framework is removed
-
-
-CreateLaunchd (){
-    echo "<?xml version="1.0" encoding="UTF-8"?>
-    <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
-    <plist version="1.0">
-    <dict>
-        <key>Disabled</key>
-        <false/>
-        <key>Label</key>
-        <string>tmp_removeframework</string>
-        <key>ProgramArguments</key>
-        <array>
-            <string>$jamf_binary</string>
-            <string>removeFramework</string>
-        </array>
-        <key>RunAtLoad</key>
-        <false/>
-        <key>StartInterval</key>
-        <integer>60</integer>
-    </dict>
-    </plist>" > /private/tmp/removetools.plist
-
-    chown root:wheel /private/tmp/removetools.plist
-    chmod 644 /private/tmp/removetools.plist 
-
-
-    /bin/launchctl load /private/tmp/removetools.plist
-}
-
 CheckBinary
 RunRecon
-UpdateAPI
-CreateLaunchd
+
+# Remove framework
+$jamf_binary removeFramework
+echo "Jamf has been removed from this Mac!"
